672a684356757a5501e63ee765e792414a1805c6
Squareben1 <bengittins@live.com>
2021-01-25 23:13:05 +0000

    Add test to check for false array

--- spec/getMultiStocks.spec.js
+++ spec/getMultiStocks.spec.js
@@ -4,7 +4,7 @@ const multiStockModule = require('../src/getMultiStocks');
 
 jest.mock('../src/StockChecker')
 
-const mockPriceDataOver5 = {
+const mockPriceData = {
   currencySymbol: '$',
   currency: 'USD',
   regularMarketPrice: { raw: 192.4, fmt: '182.78' },
@@ -13,14 +13,26 @@ const mockPriceDataOver5 = {
 }
 
 const mockPriceDataArray = [
-  mockPriceDataOver5,
-  mockPriceDataOver5
+  mockPriceData,
+  mockPriceData
 ]
 
-it('iterates over array of priceData Objects', () => {
-  const mockCheckPercent = jest.fn();
-  StockChecker.prototype.checkPercent = mockCheckPercent;
-  mockCheckPercent.mockReturnValue('VTWIX is down by -5.00%; $182.78');
-  expected = ['VTWIX is down by -5.00%; $182.78', 'VTWIX is down by -5.00%; $182.78']
-  expect(multiStockModule.getMultiStocks(mockPriceDataArray)).toMatchObject(expected)
+describe('getMultiStocks', () => {
+  it('iterates over array of priceData Objects', () => {
+    const mockCheckPercent = jest.fn();
+    StockChecker.prototype.checkPercent = mockCheckPercent;
+    mockCheckPercent.mockReturnValue('VTWIX is down by -5.00%; $182.78');
+
+    expected = ['VTWIX is down by -5.00%; $182.78', 'VTWIX is down by -5.00%; $182.78']
+    expect(multiStockModule.getMultiStocks(mockPriceDataArray)).toMatchObject(expected)
+  })
+
+  it('returns false if none of the multi objects over threshold', () => {
+    const mockCheckPercent = jest.fn();
+    StockChecker.prototype.checkPercent = mockCheckPercent;
+    mockCheckPercent.mockReturnValue(false);
+    expect(multiStockModule.getMultiStocks(mockPriceDataArray)).toMatchObject([false, false])
+  })
 })
+
+

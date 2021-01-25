6e3b55de4c50762ab4d92c6c1141ce00de724e4f
Squareben1 <bengittins@live.com>
2021-01-25 22:38:20 +0000

    Refactor getMultiStocks

--- src/getMultiStocks.js
+++ src/getMultiStocks.js
@@ -1,16 +1,7 @@
 const StockChecker = require('./StockChecker')
 
 getMultiStocks = (array) => {
-
-  const checkedStringsArray = []
-
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i].symbol)
-    checker = new StockChecker(array[i])
-    checkedStringsArray.push(checker.checkPercent())
-  }
-  console.log(checkedStringsArray)
-  return checkedStringsArray
+  return array.map(x => new StockChecker(x).checkPercent())
 }
 
 module.exports = { getMultiStocks }

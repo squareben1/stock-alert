533336ec5ee3fefb3a5a658599f72f49bb61aca7
Squareben1 <bengittins@live.com>
2021-02-06 19:51:27 +0000

    Remove zip

--- index.js
+++ index.js
@@ -5,31 +5,17 @@ const sendSMS = require('./src/sendSMS')
 
 // TODO: add here? - const defaultMarketChangePercent = -5
 
-// const targetArray = [
-//   {
-//     // Vanguard FTSA Global All Cap Acc
-//     symbol: '0P00018XAR.L',
-//     region: "en"
-//   },
-//   {
-//     symbol: 'btc-gbp',
-//     region: "en"
-//   },
-//   {
-//     symbol: 'DNLM.L',
-//     region: "en"
-//   },
-// ]
-
-// need to refactor to accomodate JSON only event data w/ multi target stocks
-
 exports.handler = async (event) => {
-  const targetArray = [event]
+  const targetArray = event
   console.log("event:", event)
   console.log("targetArray", targetArray)
-  const priceDataArray = await priceDataModule.getMultiplePriceData(targetArray)
-  const stringArray = multiStockModule.getMultiStocks(priceDataArray, -0.5)
-  const joinedResultString = multiStockModule.filterJoinArray(stringArray)
+  try {
+    const priceDataArray = await priceDataModule.getMultiplePriceData(targetArray)
+    const stringArray = multiStockModule.getMultiStocks(priceDataArray, -0.5)
+    var joinedResultString = multiStockModule.filterJoinArray(stringArray)
+  } catch (e) {
+    console.log("Error:", e)
+  }
   console.log('joinedResultString', joinedResultString)
 
   if (joinedResultString.length > 0) {

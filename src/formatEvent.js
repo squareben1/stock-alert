e19b29ffc822bc8ecf1682450e7ad56c103e981d
Squareben1 <bengittins@live.com>
2021-02-11 21:34:18 +0000

    Confirmation test & reformat formatEvent

--- src/formatEvent.js
+++ src/formatEvent.js
@@ -1,15 +1,6 @@
 
 stripMarketPercent = (arr) => {
-  // const arr = JSON.parse(array)
-  // // const targetArray = []
-  for (i = 0; i < arr.length; i++) {
-    delete arr[i].marketChangePercent
-    // targetArray.append()
-    console.log('HEHREEHDHD:', arr[i])
-  }
-
-  return arr
-  // return arr.map(x => delete x['marketChangePercent'])
+  return arr.filter(i => i !== "marketChangePercent")
 }
 
 module.exports = { stripMarketPercent }
